2d890813fe0ed841b91c6d54f250e264e12c2231
jessicatanhc <jessicatan_hc@hotmail.com>
2019-11-12 10:44:55 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,7 +21,8 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
-function checkForMatch() {
+function checkForMatch(cardId) {
+	this.setAttribute("src",cards[cardId].cardImage);
 	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]){
 			alert("You found a match!");
@@ -30,13 +31,23 @@ function checkForMatch() {
 		}
 	}
 }
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	checkForMatch.bind(this)(cardId);
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+createBoard();
+
 
